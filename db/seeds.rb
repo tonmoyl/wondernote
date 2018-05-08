67eb710dba6e3aee7b649a67322db7d1fd8803fd
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-05-08 14:49:48 -0400

    Successful ajax request

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+user1 = User.create(username: 'Lifazul', password: 'starwars')
+user2 = User.create(username: 'Tonmoy', password: 'starwars')
