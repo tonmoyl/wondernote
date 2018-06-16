6a083577b8c269461847710648943004562031dd
Lifazul Tonmoy <tonmoy.lifazul@gmail.com>
2018-06-16 14:48:57 -0400

    Successfully deletes all the notes and removes them from the state if parent notebook is deleted

--- frontend/reducers/notes_reducer.js
+++ frontend/reducers/notes_reducer.js
@@ -1,32 +1,33 @@
 import merge from 'lodash/merge';
 
 import { RECEIVE_NOTE, RECEIVE_NOTES, REMOVE_NOTE } from '../actions/note_actions';
+import { DELETE_NOTEBOOK } from '../actions/notebook_actions';
 import { LOGOUT_CURRENT_USER } from '../actions/session_actions';
 
 const predefinedState = {};
 const notesReducer = (state = predefinedState, action) => {
   Object.freeze(state);
+  let newState;
   switch(action.type) {
     case RECEIVE_NOTES:
       return merge({}, state, action.notes);
     case RECEIVE_NOTE:
       const newNote = { [action.note.id]: action.note };
-      // let middleState = merge({}, state)
-      // let noteIds = Object.keys(state);
-      // for (let i = 0; i < noteIds.length; i++) {
-      //   if (middleState[noteIds[i]] == action.note.id) {
-      //
-      //     delete middleState[noteIds[i]];
-      //   }
-      // }
-
       return merge({}, state, newNote);
     case REMOVE_NOTE:
-      const newState = merge({}, state);
+      newState = merge({}, state);
       delete newState[action.noteId];
       return newState;
     case LOGOUT_CURRENT_USER:
       return predefinedState;
+    case DELETE_NOTEBOOK:
+      newState = merge({}, state);
+      Object.keys(newState).forEach( (noteId) => {
+        if (newState[noteId].notebook_id === action.notebookID) {
+          delete newState[noteId];
+        }
+      })
+      return newState;
     default:
       return state;
   }
