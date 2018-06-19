e14b66a181d30872e68966aa55706f89b31bfdd8
Lifazul Tonmoy <tonmoy.lifazul@gmail.com>
2018-06-19 13:59:57 -0400

    View all the notebooks on the search form

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Search extends React.Component {
   constructor(props) {
     super(props)
+    this.filterFunction.bind(this);
+  }
+
+  filterFunction() {
+
   }
 
   render() {
-
+    var notes = this.props.notes;
+    let renderNotes = Object.keys(this.props.notes).map( (id) => {
+      return (
+        <li key={id}>
+          <Link to={`/main/${id}`}>{this.props.notes[id].title}</Link>
+        </li>
+      );
+    })
 
     return (
       <div id="search-form" className="dropdown">
@@ -14,8 +27,10 @@ export default class Search extends React.Component {
           type="text"
           placeholder="Search Notes.."
           id="myInput"
-          onkeyup="filterFunction()"
-          />
+          ></input>
+        <ul>
+          {renderNotes}
+        </ul>
       </div>
     )
   }
