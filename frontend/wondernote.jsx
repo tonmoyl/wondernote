348f287c831e7f7591502dcfb86c07de7f5df5e5
Lifazul Tonmoy <tonmoy.lifazul@gmail.com>
2018-06-21 16:25:54 -0400

    Seeing if contribution pops up on github

--- frontend/wondernote.jsx
+++ frontend/wondernote.jsx
@@ -10,12 +10,6 @@ window.signup = signup;
 window.logout = logout;
 //test
 
-//test
-import { createNote, fetchNotes } from './util/note_api_util';
-window.createNote = createNote;
-window.fetchNotes = fetchNotes;
-//test
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store;
