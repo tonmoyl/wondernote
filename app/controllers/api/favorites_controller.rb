19354110fd3abad7514ca23c4cb6da11f4dc5454
Lifazul Tonmoy <tonmoy.lifazul@gmail.com>
2018-06-21 18:17:12 -0400

    Create and destroy functions in Fav Controller

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,15 +1,28 @@
 class Api::FavoritesController < ApplicationController
   def create
-
+    @favorite = Favorite.new(favorite_params)
+    @favorite.user_id = current_user.id
+    if @favorite.save
+      render json @favorite
+    else
+      render json @favorite.errors.full_messages, status: 422
+    end
   end
 
   def destroy
-
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
   end
 
   def index
     @favorites = User.first.favorite_notes
     render :index
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:note_id)
   end
 
 end
