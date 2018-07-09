cbbc7d1b3d8735370e10524ec9370a721c61f70f
Lifazul Tonmoy <tonmoy.lifazul@gmail.com>
2018-07-09 16:13:30 -0400

    Sidebars will close if another sidebar is opened

--- frontend/util/ui_util.js
+++ frontend/util/ui_util.js
@@ -1,6 +1,13 @@
+var closeOtherWindows = () => {
+  document.getElementById("notebook-side").style.width = "0px";
+  document.getElementById("favorite-index").style.width = "0px";
+  document.getElementById("search-form").style.width = "0px";
+}
+
 export const toggleNavNotebook = () => {
   const sidebarWidth = document.getElementById("notebook-side").style.width;
   if (sidebarWidth === "0px") {
+    closeOtherWindows();
     document.getElementById("notebook-side").style.width = "430px";
   } else {
     document.getElementById("notebook-side").style.width = "0px";
@@ -10,6 +17,7 @@ export const toggleNavNotebook = () => {
 export const toggleFavorites = () => {
   const sidebarWidth = document.getElementById("favorite-index").style.width;
   if (sidebarWidth === "0px") {
+    closeOtherWindows();
     document.getElementById("favorite-index").style.width = "430px";
   } else {
     document.getElementById("favorite-index").style.width = "0px";
@@ -19,6 +27,7 @@ export const toggleFavorites = () => {
 export const toggleSearch = () => {
   const sidebarZ = document.getElementById("search-form").style.width;
   if (sidebarZ === "0px") {
+    closeOtherWindows();
     document.getElementById("search-form").style.width = "100%";
     // document.getElementById("search-form").style.visibility = "visible";
   } else {
