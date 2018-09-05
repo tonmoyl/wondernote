e9b9ae9fc01f1b7807b3d68fc09b299086244822
Lifazul Tonmoy <tonmoy.lifazul@gmail.com>
2018-09-05 18:57:59 -0400

    Sorted through alphabetically works

--- frontend/components/note/note_index.jsx
+++ frontend/components/note/note_index.jsx
@@ -5,7 +5,12 @@ import NoteItemContainer from './note_item_container';
 export default class NoteIndex extends React.Component{
   constructor(props) {
     super(props);
-
+    this.state = {
+      order: "update",
+      noteIds: this.props.noteIds
+    }
+    this.updateOrder = this.updateOrder.bind(this);
+    this.sortAlphabetical = this.sortAlphabetical.bind(this);
   }
 
   componentWillMount(){
@@ -15,23 +20,76 @@ export default class NoteIndex extends React.Component{
     }
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    this.state.noteIds = nextProps.noteIds;
+    return true;
+  }
+
+  updateOrder(){
+    const order = document.getElementById("dropdown-order").value;
+
+    switch(order){
+      case "update":
+        console.log("hit switch update");
+        break
+      case "alphabetical":
+        console.log("hit asdl");
+        this.sortAlphabetical()
+        break
+      case "reverse-alphabetical":
+        console.log("saddsad");
+        break
+      default:
+        console.log("default");
+        break
+    }
+    this.state.order = order;
+  }
+
+  sortAlphabetical() {
+    let notes = this.props.notes;
+    let sorted = [];
+
+    for (var id in this.props.notes) {
+      sorted.push([this.props.notes[id].title, id]);
+    }
+
+    function comparator(a,b) {
+      if (a[0].toLowerCase() < b[0].toLowerCase()) return -1;
+      if (a[0].toLowerCase() > b[0].toLowerCase()) return 1;
+      if (a[0].toLowerCase() === b[0].toLowerCase()) return 0;
+    }
+
+
+    sorted = sorted.sort(comparator);
+    sorted = sorted.map( (item) => {
+      return item[1];
+    });
+    this.setState({noteIds: sorted});
+    console.log(this.state.noteIds);
+  }
+
   render(){
     let count = 0;
-
-    const notes = this.props.noteIds.map( (id) => {
-      count ++;
-      return (
-        <li key={id}>
-          <NoteItemContainer
-            note={this.props.notes[id]}
-            prevLink={"noteIndex"}
-            parentComponent="noteIndex"/>
-        </li>
+    let notes = this.state.noteIds.map( (id) => {
+        count ++;
+        return (
+          <li key={id}>
+            <NoteItemContainer
+              note={this.props.notes[id]}
+              prevLink={"noteIndex"}
+              parentComponent="noteIndex"/>
+          </li>
         );
-    });
+      });
 
     return(
       <div id={this.props.componentType} className={this.props.componentType}>
+        <select id="dropdown-order" className="dropdown-order" onChange={this.updateOrder}>
+          <option className="value-order" value="update">Last Updated</option>
+          <option className="value-order" value="alphabetical">Sort by Title (a-z)</option>
+          <option className="value-order" value="reverse-alphabetical">Sort by Title (z-a)</option>
+        </select>
         <div className="note-index-header">
           <h2>{this.props.componentTitle}</h2>
           <div className="noteslist-footer">
